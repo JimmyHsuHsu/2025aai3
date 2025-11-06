a8d39c7418abeabaaa3e90a455bb87d587111532
JimmyHsuHsu <cccjmcheng@gmail.com>
2025-11-06 17:59:27 +0800

    week09

---
+++ week09/week09-1b.cpp
@@ -0,0 +1,15 @@
+// week09-1b.cpp
+// SOIT106 ADVANCE 004
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    while( cin >> c ){
+        if(c>='A' && c<='Z') c = c - 'A' + 'a';
+        else if(c>='a'&&c<='z') c = c - 'a' + 'A';
+        cout << c;
+    }
+    cout << "\n";
+}
